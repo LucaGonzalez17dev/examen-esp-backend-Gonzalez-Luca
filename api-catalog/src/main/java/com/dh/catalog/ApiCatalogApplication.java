ded6834e7796b93d3aff1863ef7d0c2c1c8d32cb
LucaGonzalez17dev <lucadxtx17@gmail.com>
2023-06-22 15:09:51 -0300

    se agrega Zipkin a la solución

--- api-catalog/src/main/java/com/dh/catalog/ApiCatalogApplication.java
+++ api-catalog/src/main/java/com/dh/catalog/ApiCatalogApplication.java
@@ -1,7 +1,9 @@
 package com.dh.catalog;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
--- api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
+++ api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
@@ -27,4 +27,6 @@ public interface MovieServiceClient {
 		private String urlStream;
 	}
 
+
+
 }

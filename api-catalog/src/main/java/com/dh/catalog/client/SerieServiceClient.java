5db043c2e00e402f77277659e56634c4ff058b68
LucaGonzalez17dev <lucadxtx17@gmail.com>
2023-06-26 12:51:50 -0300

    se agrega circuit braker al metodo get de series en catalog

--- api-catalog/src/main/java/com/dh/catalog/client/SerieServiceClient.java
+++ api-catalog/src/main/java/com/dh/catalog/client/SerieServiceClient.java
@@ -1,21 +1,30 @@
 package com.dh.catalog.client;
 
-import lombok.AllArgsConstructor;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @FeignClient(name= "api-serie" )
 public interface SerieServiceClient {
 
     @GetMapping("/api/v1/series/{genre}")
+    @CircuitBreaker(name = "serieService", fallbackMethod = "fallbackGetSerieByGenre")
     List<SerieDto> getSerieByGenre(@PathVariable (value = "genre") String genre);
+
+    default List<SerieDto> fallbackGetSerieByGenre(String genre, CallNotPermittedException exception) {
+        SerieDto fallbackSerie = new SerieDto();
+        fallbackSerie.setName("Fallback Series");
+        return Collections.singletonList(fallbackSerie);
+    }
+
 
     @Getter
     @Setter
